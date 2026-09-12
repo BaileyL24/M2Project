885704197c3bb9abea6c5b51f934f70fa57e24cd
Bailey Lester <lestbw@students.fscj.edu>
2026-09-12 11:55:28 -0400

    Task Management

---
+++ src/main/java/edu/fscj/cen3024c/taskmanager/Task.java
@@ -0,0 +1,64 @@
+package edu.fscj.cen3024c.taskmanager;
+
+public class Task {
+    private Integer id;
+    private String title;
+    private String description;
+    private String status;
+    private String dueDate;
+
+    public Task() {
+    }
+
+    public Task(Integer id, String title, String description, String status, String dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Task(Integer id) {
+        this.id = id;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
